d15fb4e02f98b22361d572aae127d18e96218df8
Hacky <Jon.Nguyen7@protonmail.com>
2022-12-19 13:46:19 -0600

    added comments for keymaps

--- .config/nvim/lua/hacky/remap.lua
+++ .config/nvim/lua/hacky/remap.lua
@@ -1,8 +1,16 @@
+-- Set the space key as leaderkey
 vim.g.mapleader = " "
+
+-- Jump to default vim file navigation view
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Open Tagbar
 vim.keymap.set("n", "<leader>tb", vim.cmd.TagbarToggle)
 
 -- Python
+-- Run Python Code
 vim.keymap.set("n", "<leader>pp", ":w<CR>:exec '!python3' shellescape(@%, 1)<CR>")
+
+-- Markdown
+-- Toggle Live Preview
+vim.keymap.set("n", "<leader>md", vim.cmd.MarkdownPreviewToggle)
