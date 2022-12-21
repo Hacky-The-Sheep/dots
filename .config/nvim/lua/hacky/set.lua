8e73524073bfbb74ebd18ae1127084c9aa489c61
Hacky <Jon.Nguyen7@protonmail.com>
2022-12-21 10:52:48 -0600

    added comments

--- .config/nvim/lua/hacky/set.lua
+++ .config/nvim/lua/hacky/set.lua
@@ -2,6 +2,7 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Spacing
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -9,11 +10,13 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
+-- Colors
 vim.opt.termguicolors = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Set space as leader
 vim.g.mapleader = " "
 
 vim.opt.cursorline = true
