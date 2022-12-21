6af2ed6bbaaf8ef0ddbb946dec6bc51adcfa04a8
Hacky <Jon.Nguyen7@protonmail.com>
2022-12-21 10:48:50 -0600

    restored defaults for errors

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,3 +1,15 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+
 lsp.setup()
+
+
+-- Show diagnostics defaults
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = false,
+    float = true
+})
