69dca1b5a10f6e3aab3677f07eeb0ed772f5fd18
Hacky <Jon.Nguyen7@protonmail.com>
2023-02-10 00:37:42 -0600

    added formatting for rust/py/prettier

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -1,50 +1,59 @@
--- Utilities for creating configurations
-local util = require "formatter.util"
+local util = require("formatter.util")
 
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup {
-  -- Enable or disable logging
-  logging = true,
-  -- Set the log level
-  log_level = vim.log.levels.WARN,
-  -- All formatter configurations are opt-in
-  filetype = {
-    -- Formatter configurations for filetype "lua" go here
-    -- and will be executed in order
-    lua = {
-      -- "formatter.filetypes.lua" defines default configurations for the
-      -- "lua" filetype
-      require("formatter.filetypes.lua").stylua,
+require("formatter").setup({
+	logging = true,
+	log_level = vim.log.levels.WARN,
+	filetype = {
+		-- Formatter configurations for filetype "lua" go here
+		-- and will be executed in order
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+			function()
+				-- Supports conditional formatting
+				if util.get_current_buffer_file_name() == "special.lua" then
+					return nil
+				end
 
-      -- You can also define your own configuration
-      function()
-        -- Supports conditional formatting
-        if util.get_current_buffer_file_name() == "special.lua" then
-          return nil
-        end
+				-- Full specification of configurations is down below and in Vim help
+				-- files
+				return {
+					exe = "stylua",
+					args = {
+						"--search-parent-directories",
+						"--stdin-filepath",
+						util.escape_path(util.get_current_buffer_file_path()),
+						"--",
+						"-",
+					},
+					stdin = true,
+				}
+			end,
+		},
+		rust = {
+			require("formatter.filetypes.rust").rustfmt,
+		},
+		css = {
+			require("formatter.filetypes.css").prettier,
+		},
+		json = {
+			require("formatter.filetypes.json").prettier,
+		},
+		html = {
+			require("formatter.filetypes.html").prettier,
+		},
+		markdown = {
+			require("formatter.filetypes.markdown").prettier,
+		},
+        python = {
+            require("formatter.filetypes.python").black,
+        },
 
-        -- Full specification of configurations is down below and in Vim help
-        -- files
-        return {
-          exe = "stylua",
-          args = {
-            "--search-parent-directories",
-            "--stdin-filepath",
-            util.escape_path(util.get_current_buffer_file_path()),
-            "--",
-            "-",
-          },
-          stdin = true,
-        }
-      end
-    },
-
-    -- Use the special "*" filetype for defining formatter configurations on
-    -- any filetype
-    ["*"] = {
-      -- "formatter.filetypes.any" defines default configurations for any
-      -- filetype
-      require("formatter.filetypes.any").remove_trailing_whitespace
-    }
-  }
-}
+		-- Use the special "*" filetype for defining formatter configurations on
+		-- any filetype
+		["*"] = {
+			-- "formatter.filetypes.any" defines default configurations for any
+			-- filetype
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+	},
+})
